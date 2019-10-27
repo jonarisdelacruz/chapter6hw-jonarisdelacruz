135534fbdd7ed933d90499dc99e08cd5c227cc53
jonaris <jonaris@DESKTOP-3DMBAR8>
2019-10-27 14:45:34 -0400

    Add project files.

---
+++ CornMazeAverageTime/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCornMaze
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmCornMaze))
+        Me.picMazeEntrance = New System.Windows.Forms.PictureBox()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblaverage = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnEnterTime = New System.Windows.Forms.Button()
+        CType(Me.picMazeEntrance, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'picMazeEntrance
+        '
+        Me.picMazeEntrance.Image = CType(resources.GetObject("picMazeEntrance.Image"), System.Drawing.Image)
+        Me.picMazeEntrance.Location = New System.Drawing.Point(408, 38)
+        Me.picMazeEntrance.Name = "picMazeEntrance"
+        Me.picMazeEntrance.Size = New System.Drawing.Size(248, 413)
+        Me.picMazeEntrance.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picMazeEntrance.TabIndex = 0
+        Me.picMazeEntrance.TabStop = False
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Location = New System.Drawing.Point(84, 38)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(190, 17)
+        Me.lblHeading.TabIndex = 1
+        Me.lblHeading.Text = "Race through the Corn Maze"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(81, 153)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(165, 100)
+        Me.ListBox1.TabIndex = 3
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(679, 28)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(118, 26)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(118, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'lblaverage
+        '
+        Me.lblaverage.AutoSize = True
+        Me.lblaverage.Location = New System.Drawing.Point(41, 341)
+        Me.lblaverage.Name = "lblaverage"
+        Me.lblaverage.Size = New System.Drawing.Size(205, 17)
+        Me.lblaverage.TabIndex = 5
+        Me.lblaverage.Text = "The average completion time is"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(96, 270)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(107, 23)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnEnterTime
+        '
+        Me.btnEnterTime.Location = New System.Drawing.Point(15, 97)
+        Me.btnEnterTime.Name = "btnEnterTime"
+        Me.btnEnterTime.Size = New System.Drawing.Size(321, 23)
+        Me.btnEnterTime.TabIndex = 7
+        Me.btnEnterTime.Text = "Please Enter up to 10 corn maze completion times"
+        Me.btnEnterTime.UseVisualStyleBackColor = True
+        '
+        'frmCornMaze
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(679, 487)
+        Me.Controls.Add(Me.btnEnterTime)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblaverage)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.picMazeEntrance)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmCornMaze"
+        Me.Text = "Corn Maze Average Time"
+        CType(Me.picMazeEntrance, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picMazeEntrance As PictureBox
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents lblaverage As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnEnterTime As Button
+End Class
---
+++ CornMazeAverageTime/Form1.vb
@@ -0,0 +1,69 @@
+﻿'Jonaris De la Cruz 
+'Cpt 182
+'Chapter 6
+'This program let the user enter 10 values representing the number of minutes that 
+'they completed the corn maze successfully
+
+Public Class frmCornMaze
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        'close the application
+        Close()
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        'clear all text entered
+        ListBox1.Items.Clear()
+        lblaverage.Text = ""
+
+    End Sub
+
+    Private Sub btnEnterTime_Click(sender As Object, e As EventArgs) Handles btnEnterTime.Click
+        'declaration of variables
+
+        Dim dectotalTime As Decimal = 0D
+        Dim strInputHeading As String = "completion times"
+
+
+        Dim strbtnEnterTime As String
+        Dim decEnterTime As Decimal
+        Dim decaveragetime As Decimal
+        Dim strInputMessage As String = "Please enter time #"
+        Dim strnormalmessage As String = "please enter time #"
+        Dim strNegativeError As String = "Error- enter a positive number "
+        Dim intMaxNumberOfEntries As Integer = 10
+        Dim intNumberOfEntries As Integer = 1
+        'input
+        strbtnEnterTime = InputBox(strInputMessage & intNumberOfEntries, "")
+        'loop
+        Do Until intNumberOfEntries > intMaxNumberOfEntries
+
+            If IsNumeric(strbtnEnterTime) Then
+                decEnterTime = Integer.Parse(strbtnEnterTime)
+                If decEnterTime > 0 Then
+                    ListBox1.Items.Add(decEnterTime)
+                    dectotalTime += decEnterTime
+                    intNumberOfEntries += 1
+                    strInputMessage = strnormalmessage
+                Else
+                    strInputMessage = strNegativeError
+                End If
+            Else
+                strInputMessage = strNegativeError
+            End If
+            If intNumberOfEntries <= intMaxNumberOfEntries Then
+                strbtnEnterTime = InputBox(strInputMessage & intNumberOfEntries, strInputHeading)
+            End If
+        Loop
+        'output of the program
+        If intNumberOfEntries > 1 Then
+            lblaverage.Visible = True
+            decaveragetime = dectotalTime / (intNumberOfEntries - 1)
+            lblaverage.Text = "The Average Completion time is " & decaveragetime.ToString("f1") & " minutes"
+        Else
+            MsgBox("no time entered")
+
+        End If
+
+    End Sub
+End Class
